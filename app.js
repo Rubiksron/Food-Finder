246f39da6560e6a3bc2f83b1335490be30c9ef38
Rubiksron <rubiksron@gmail.com>
2017-11-22 09:03:08 -0800

    trying to get around 'no access control header' error message by trying out my get request while not on localhost.

--- app.js
+++ app.js
@@ -14,28 +14,32 @@ let map;
        document.getElementsByTagName('head')[0].appendChild(script);
      }
 
-     window.eqfeed_callback = function(results) {
-       for (let i = 0; i < results.features.length; i++) {
-         let place = results.features[i].properties.place;
-         let coords = results.features[i].geometry.coordinates;
-         let latLng = new google.maps.LatLng(coords[1],coords[0]);
-         let marker = new google.maps.Marker({
-           position: latLng,
-           map: map
-         });
-         let resultsEl = document.getElementById('results');
-         let hrEl = document.createElement('hr');
-         hrEl.textContent = place;
-         resultsEl.appendChild(hrEl);
-       }
-       resultsArray.push(results)
-       console.log(' inside callback  --  resultsArray[0].features[1].properties.place:  ', resultsArray[0].features[1].properties.place);
-     }
-     console.log('outside callback  --  resultsArray', resultsArray);
+     // window.eqfeed_callback = function(results) {
+     //   for (let i = 0; i < results.features.length; i++) {
+     //     let place = results.features[i].properties.place;
+     //     let coords = results.features[i].geometry.coordinates;
+     //     let latLng = new google.maps.LatLng(coords[1],coords[0]);
+     //     let marker = new google.maps.Marker({
+     //       position: latLng,
+     //       map: map
+     //     });
+     //     let resultsEl = document.getElementById('results');
+     //     let hrEl = document.createElement('hr');
+     //     hrEl.textContent = place;
+     //     resultsEl.appendChild(hrEl);
+     //   }
+     //   resultsArray.push(results)
+     //   console.log(' inside callback  --  resultsArray[0].features[1].properties.place:  ', resultsArray[0].features[1].properties.place);
+     // }
+     // console.log('outside callback  --  resultsArray', resultsArray);
+     //the log above cannot dig any deeper into the array i.e. no resultsArray[0].features
 
-
-
-
+     $.get('https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js')
+       .then(
+         data => (data).forEach(repo =>
+           $('#results').append(`<h3>${repo.name}</h3><p>${repo.description}</p><h3>${repo.url}</h3><hr>`)),
+         err => console.error(err.status, err.statusText, 'is the way my stuff is broken')
+       );
 
      // the below format works for adding a class
      // hrEl.setAttribute("class", "green");
